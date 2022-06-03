fce6e4d5fe57692d3e28ba18f21cfa7534565911
David Antonio <44716806+DavidAntonio317@users.noreply.github.com>
2022-06-04 03:44:43 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -3,7 +3,7 @@ import socket
 import hashlib
 import math
 import time
-import argparse
+import sys
 
 # Checksum function
 def checksum(packet):
@@ -12,30 +12,34 @@ def checksum(packet):
 # Initialize main function
 def main():
     # Parsing commandline arguments
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument('-f', default='e3e22884.txt')
-    argparser.add_argument('-a', default='10.0.7.141')
-    argparser.add_argument('-s', default=9000)
-    argparser.add_argument('-c', default=6671)
-    argparser.add_argument('-i', default='e3e22884')
-    args = argparser.parse_args()
+    args = sys.argv
+    filename = "e3e22884.txt"
+    HOST = "10.0.7.141"
+    PORT = 9000
+    src_port = 6671
+    ID = "e3e22884"
 
-    filename = args.f 
-    dest_host = args.a
-    dest_port = int(args.s)
-    src_port = int(args.c)
-    ID = args.i  
+    for i in range(len(args)):
+        if args[i] == "-f":
+            filename = args[i+1]
+        elif args[i] == "-a":
+            HOST = args[i+1]
+        elif args[i] == "-s":
+            PORT = int(args[i+1])
+        elif args[i] == "-c":
+            src_port = int(args[i+1])
+        elif args[i] == "-i":
+            ID = args[i+1]
 
-    # Initiate UDP connection
+    # Initialization of  UDP connection
     UDP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Bind the UDP scoket to host and port
+    # Binding the UDP socket to host and port
     UDP_SOCKET.bind(('', src_port))
     UDP_SOCKET.settimeout(11)
 
-
     # Send an Intent Message
-    UDP_SOCKET.sendto(f'ID{ID}'.encode(), (dest_host, dest_port))
+    UDP_SOCKET.sendto(f'ID{ID}'.encode(), (HOST, PORT))
 
     # Receive Acknowledgement
     data, addr = UDP_SOCKET.recvfrom(1024)
@@ -47,63 +51,56 @@ def main():
     # Open payload.txt
     FILE = open(filename)
     payload = FILE.read()
-    packet_size = max(1, math.ceil(len(payload) / 90)) # packet_size
-
+    dat_length = math.ceil(len(payload) / 95) # dat_length
     k = 0
     SN = 0
-    flag = True
     z = 0
-
-    t_start = time.time()
+    flag = True
+    initial_t = time.time()
     while k < len(payload):
-        start = time.time()
-
-        while True:
-            if k + packet_size < len(payload):
-                data = payload[k:k + packet_size]
+        start_t = time.time()
+        while (1):
+            current_k = dat_length + k
+            if current_k  < len(payload):
+                data = payload[k:current_k]
             else:
                 data = payload[k:]
 
-            if k + packet_size < len(payload):
+            if current_k < len(payload):
                 z = 0
             else:
                 z = 1
 
             packet = f'ID{ID}SN{str(SN).zfill(7)}TXN{transaction}LAST{z}{data}'
-            check = checksum(packet)
-            UDP_SOCKET.sendto(packet.encode(), (dest_host,dest_port))
+            checker = checksum(packet)
+            UDP_SOCKET.sendto(packet.encode(), (HOST,PORT))
             print(packet)
             try:
                 ack = UDP_SOCKET.recv(64).decode()
                 print(ack)
             except socket.timeout:
-                # print("got here")
-                packet_size = int(packet_size * 0.75)
+                dat_length = math.ceil(dat_length * 0.95)
                 UDP_SOCKET.settimeout(UDP_SOCKET.gettimeout() + 2)
             else:
-                # check that the ack received is for the packet that was sent
-                if ack[-32:] == check:
+                if ack[-32:] == checker:
                     break
+        k = current_k
+        SN = SN + 1
+        end_t = time.time()
+        span = end_t - start_t
+        UDP_SOCKET.settimeout(span + 2) 
 
-        end = time.time()
-        duration = end - start
-        UDP_SOCKET.settimeout(duration + 2) 
+        while flag:    
+            # Calculate the number of packets that can be sent in the time remaining
+            packetsLeft = math.ceil((95 - span) / span)  
+            # Calculate the amount of characters in each packet
+            dat_length = math.ceil((len(payload) - k) / packetsLeft)
 
-        k+=packet_size
-        SN+=1
-        if flag:
             flag = False
-            timeout = duration
-            # compute number of packets that can be sent in remaining time
-            packet_num = (90 - timeout) // timeout
-            # compute number of characters per packet
-            packet_size = math.ceil((len(payload) - k) / packet_num)
 
-
-        # print(data, duration) # for testing
-
-    t_end = time.time()
-    print(f'Transaction: {t_end - t_start}')
+    final_t = time.time()
+    transaction_t = final_t - initial_t
+    print(f'Transaction: {transaction_t}')
 
 if __name__ == "__main__":
     main()
